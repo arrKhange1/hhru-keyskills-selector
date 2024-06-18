267d40923bc5d5fc663826a5d02c3f9e4e757444
arrKhange1 <therock65frostik@gmail.com>
2024-06-18 17:50:45 +0500

    feat: Add an algo on hash map and heap

--- src/main/java/com/arrkhange1/keywordsmanager/service/SimpleAlgorithmService.java
+++ src/main/java/com/arrkhange1/keywordsmanager/service/SimpleAlgorithmService.java
@@ -1,20 +1,15 @@
 package com.arrkhange1.keywordsmanager.service;
 
 import com.arrkhange1.keywordsmanager.model.Vacancies;
-import com.arrkhange1.keywordsmanager.model.VacanciesItem;
 import com.arrkhange1.keywordsmanager.model.Vacancy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.Marker;
-import org.slf4j.MarkerFactory;
 import org.springframework.context.annotation.Primary;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+
 
 @Primary
 @Service
@@ -22,7 +17,49 @@ public class SimpleAlgorithmService implements AlgorithmService {
 
     private final Logger logger = LoggerFactory.getLogger(SimpleAlgorithmService.class);
 
+    Map<String, Integer> keywordsCounter = new HashMap<>();
+
+    PriorityQueue<Map.Entry<String, Integer>> prioritizedKeywords = new PriorityQueue<>(51, (k1, k2)
+            -> k2.getValue() - k1.getValue());
+
     public List<String> getKeywords(String jobRequest) {
+
+    // hash map
+//        all elements for O(N)
+//        sort hash map for getting first 50 for O(nlogn)
+
+        // tree map
+//        all elements for O(nlogn)
+//        take first 50 for O(1)
+
+        // linked hash map
+//        all elements for O(n)
+//        O(nlogn)
+
+        // priority queue
+//        all elements for O(nlogn)
+//        take first O(klogk) - constant
+
+//        if (!keywordsCounter.isEmpty()) {
+//
+//            for (var entry : keywordsCounter.entrySet()) {
+//                if (prioritizedKeywords.size() > 50) {
+//                    prioritizedKeywords.poll();
+//                }
+//                else {
+//                    prioritizedKeywords.add(entry);
+//                }
+//            }
+//            if (prioritizedKeywords.size() > 50) {
+//                prioritizedKeywords.poll();
+//            }
+//
+//            while (!prioritizedKeywords.isEmpty()) {
+//                logger.info(prioritizedKeywords.poll().toString());
+//            }
+//
+//            return null;
+//        }
 
         String baseUrl = "https://api.hh.ru/vacancies";
         RestClient customClient = RestClient.builder()
@@ -35,18 +72,25 @@ public class SimpleAlgorithmService implements AlgorithmService {
                 .retrieve()
                 .body(Vacancies.class);
 
+        logger.info(vacancies + " Кол-во вакансий: " + vacancies.items().size());
 
+        vacancies.items().forEach(vacanciesItem -> {
+            Vacancy vacancy = customClient.get()
+                    .uri("/{vacancy_id}", vacanciesItem.id())
+                    .retrieve()
+                    .body(Vacancy.class);
+            vacancy.key_skills().forEach(keyword -> {
+                if (keywordsCounter.containsKey(keyword.name())) {
+                    keywordsCounter.replace(keyword.name(), keywordsCounter.get(keyword.name())+1);
+                }
+                else {
+                    keywordsCounter.put(keyword.name(), 1);
+                }
+            });
+        });
 
-        logger.info(vacancies.toString());
+        logger.info(keywordsCounter + " Кол-во ключ. слов: " + keywordsCounter.size());
 
-        VacanciesItem firstVacancy = vacancies.items().getFirst();
-
-        Vacancy vacancy = customClient.get()
-                .uri("/{vacancy_id}", firstVacancy.id())
-                .retrieve()
-                .body(Vacancy.class);
-
-        logger.info(vacancy.toString());
 
         return List.of(jobRequest);
     }
